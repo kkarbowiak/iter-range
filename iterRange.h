e98e9138609400e0f1dca61205d5bb8a0cbabfb4
kkarbowiak <kkarbowiak@localhost>
2017-04-10 23:23:47 +0000

    Extend the implementation

--- iterRange.h
+++ iterRange.h
@@ -14,7 +14,7 @@ namespace iter
             class iterator
             {
                 public:
-                    explicit iterator(counter_type value);
+                    iterator(counter_type value, counter_type step);
                     
                     bool operator!=(iterator const & other) const;
                     
@@ -24,12 +24,13 @@ namespace iter
                     
                 private:
                     counter_type mValue;
+                    counter_type const mStep;
             };
 
         public:
             range_helper();
             explicit range_helper(counter_type stop);
-            range_helper(counter_type start, counter_type stop);
+            range_helper(counter_type start, counter_type stop, counter_type step = 1);
             
             iterator begin() const;
             iterator end() const;
@@ -37,13 +38,14 @@ namespace iter
         private:
             counter_type const mStart;
             counter_type const mStop;
+            counter_type const mStep;
     };
     
     template<typename C>
     range_helper<C> range(C stop);
     
     template<typename C>
-    range_helper<C> range(C start, C stop);
+    range_helper<C> range(C start, C stop, C step = 1);
 }
 
 
@@ -51,8 +53,9 @@ namespace iter
 {
 ////////////////////////////////////////////////////////////////////////////////
 template<typename C>
-inline range_helper<C>::iterator::iterator(counter_type value)
+inline range_helper<C>::iterator::iterator(counter_type value, counter_type step)
     : mValue(value)
+    , mStep(step)
 {
 }
 ////////////////////////////////////////////////////////////////////////////////
@@ -71,7 +74,7 @@ inline typename range_helper<C>::counter_type range_helper<C>::iterator::operato
 template<typename C>
 inline typename range_helper<C>::iterator & range_helper<C>::iterator::operator++()
 {
-    ++mValue;
+    mValue += mStep;
     
     return *this;
 }
@@ -87,26 +90,28 @@ template<typename C>
 inline range_helper<C>::range_helper(counter_type stop)
     : mStart(0)
     , mStop(stop)
+    , mStep(1)
 {
 }
 ////////////////////////////////////////////////////////////////////////////////
 template<typename C>
-inline range_helper<C>::range_helper(counter_type start, counter_type stop)
+inline range_helper<C>::range_helper(counter_type start, counter_type stop, counter_type step)
     : mStart(start)
     , mStop(stop)
+    , mStep(step)
 {
 }
 ////////////////////////////////////////////////////////////////////////////////
 template<typename C>
 inline typename range_helper<C>::iterator range_helper<C>::begin() const
 {
-    return iterator(mStart);
+    return iterator(mStart, mStep);
 }
 ////////////////////////////////////////////////////////////////////////////////
 template<typename C>
 inline typename range_helper<C>::iterator range_helper<C>::end() const
 {
-    return iterator(mStop);
+    return iterator(mStop, mStep);
 }
 ////////////////////////////////////////////////////////////////////////////////
 template<typename C>
@@ -116,9 +121,9 @@ inline range_helper<C> range(C stop)
 }
 ////////////////////////////////////////////////////////////////////////////////
 template<typename C>
-inline range_helper<C> range(C start, C stop)
+inline range_helper<C> range(C start, C stop, C step)
 {
-    return range_helper<C>(start, stop);
+    return range_helper<C>(start, stop, step);
 }
 ////////////////////////////////////////////////////////////////////////////////
 }
