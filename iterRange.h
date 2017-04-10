f03f58255c33d4fac944285932ae78dee07c38c9
kkarbowiak <kkarbowiak@localhost>
2017-04-10 23:07:56 +0000

    Extend the implementation

--- iterRange.h
+++ iterRange.h
@@ -9,20 +9,72 @@ namespace iter
     {
         public:
             typedef C counter_type;
+            
+        public:
+            class iterator
+            {
+                public:
+                    explicit iterator(counter_type value);
+                    
+                    bool operator!=(iterator const & other) const;
+                    
+                    counter_type operator*() const;
+                    
+                    iterator & operator++();
+                    
+                private:
+                    counter_type mValue;
+            };
 
         public:
             range_helper();
             explicit range_helper(counter_type stop);
             range_helper(counter_type start, counter_type stop);
+            
+            iterator begin() const;
+            iterator end() const;
 
         private:
             counter_type const mStart;
             counter_type const mStop;
     };
+    
+    template<typename C>
+    range_helper<C> range(C stop);
+    
+    template<typename C>
+    range_helper<C> range(C start, C stop);
 }
+
 
 namespace iter
 {
+////////////////////////////////////////////////////////////////////////////////
+template<typename C>
+inline range_helper<C>::iterator::iterator(counter_type value)
+    : mValue(value)
+{
+}
+////////////////////////////////////////////////////////////////////////////////
+template<typename C>
+inline bool range_helper<C>::iterator::operator!=(range_helper<C>::iterator const & other) const
+{
+    return (mValue != other.mValue);
+}
+////////////////////////////////////////////////////////////////////////////////
+template<typename C>
+inline typename range_helper<C>::counter_type range_helper<C>::iterator::operator*() const
+{
+    return mValue;
+}
+////////////////////////////////////////////////////////////////////////////////
+template<typename C>
+inline typename range_helper<C>::iterator & range_helper<C>::iterator::operator++()
+{
+    ++mValue;
+    
+    return *this;
+}
 ////////////////////////////////////////////////////////////////////////////////
 template<typename C>
 inline range_helper<C>::range_helper()
@@ -43,6 +95,30 @@ inline range_helper<C>::range_helper(counter_type start, counter_type stop)
     : mStart(start)
     , mStop(stop)
 {
+}
+////////////////////////////////////////////////////////////////////////////////
+template<typename C>
+inline typename range_helper<C>::iterator range_helper<C>::begin() const
+{
+    return iterator(mStart);
+}
+////////////////////////////////////////////////////////////////////////////////
+template<typename C>
+inline typename range_helper<C>::iterator range_helper<C>::end() const
+{
+    return iterator(mStop);
+}
+////////////////////////////////////////////////////////////////////////////////
+template<typename C>
+inline range_helper<C> range(C stop)
+{
+    return range_helper<C>(stop);
+}
+////////////////////////////////////////////////////////////////////////////////
+template<typename C>
+inline range_helper<C> range(C start, C stop)
+{
+    return range_helper<C>(start, stop);
 }
 ////////////////////////////////////////////////////////////////////////////////
 }
